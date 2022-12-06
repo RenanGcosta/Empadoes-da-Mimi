3e2b8323ca9561ce4f7856055f63e574c08b1e8b
adisoncg <adison-cg@hotmail.com>
2022-12-06 19:48:44 -0300

    melhoras no front

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -5,7 +5,8 @@
 @section('bars')
     <div class="container-fluid shadow bg-white p-4">
         <h1 class="mb-5">Alterar dados do cliente</h1>
-        <form class="row g-4" method="post" action="{{ route('clientes.update', $cliente->id) }}" enctype="multipart/form-data">
+        <form class="row g-4" method="post" action="{{ route('clientes.update', $cliente->id) }}"
+            enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <div class="row mt-5 mb-4">
@@ -19,8 +20,8 @@
                 <div class="col">
                     <div>
                         <label for="telefone" class="form-label">Telefone</label>
-                        <input type="text" name="telefone" class="form-control form-control-lg bg-light" value="{{ $cliente->telefone }}"
-                            placeholder="(DDD) XXXXX-XXXX" required>
+                        <input type="text" name="telefone" class="form-control form-control-lg bg-light"
+                            value="{{ $cliente->telefone }}" placeholder="(DDD) XXXXX-XXXX" required>
                     </div>
                 </div>
             </div>
@@ -29,38 +30,37 @@
                 <div class="col">
                     <div>
                         <label for="cep" class="form-label">CEP</label>
-                        <input type="text" name="cep" class="form-control form-control-lg bg-light" value="{{ $cliente->cep }}"
-                            placeholder="48000-000" required>
+                        <input type="text" name="cep" class="form-control form-control-lg bg-light"
+                            value="{{ $cliente->cep }}" placeholder="48000-000" required>
                     </div>
                 </div>
                 <div class="col">
+                    <div>
+                        <label for="logradouro" class="form-label">Logradouro</label>
+                        <input type="text" name="logradouro" class="form-control form-control-lg bg-light"
+                            value="{{ $cliente->logradouro }}" required>
+                    </div>
+                    <div class="col-4">
                         <div>
-                            <label for="logradouro" class="form-label">Logradouro</label>
-                            <input type="text" name="logradouro" class="form-control form-control-lg bg-light" value="{{ $cliente->logradouro }}"
-                                required>
-                        </div>
-                        <div class="col-4">
-                            <div>
-                                <label for="bairro" class="form-label">Bairro</label>
-                                <input type="text" name="bairro" class="form-control form-control-lg bg-light" value="{{ $cliente->bairro }}"
-                                    required>
-                            </div>
-                        </div>
-        
-                        <div class="col-3">
-                            <div>
-                                <label for="cidade" class="form-label">Cidade</label>
-                                <input type="text" name="cidade" class="form-control form-control-lg bg-light" value="{{ $cliente->cidade }}"
-                                    required>
-                            </div>
+                            <label for="bairro" class="form-label">Bairro</label>
+                            <input type="text" name="bairro" class="form-control form-control-lg bg-light"
+                                value="{{ $cliente->bairro }}" required>
                         </div>
                     </div>
-                    
+
+                    <div class="col-3">
+                        <div>
+                            <label for="cidade" class="form-label">Cidade</label>
+                            <input type="text" name="cidade" class="form-control form-control-lg bg-light"
+                                value="{{ $cliente->cidade }}" required>
+                        </div>
+                    </div>
                 </div>
+
             </div>
             <div>
-                <button type="submit" class="btn btn-danger btn-lg">Atualizar</button>
-                <a href="{{ route('clientes.index') }}" class="btn btn-success btn-lg">Cancelar</a>
+                <button type="submit" class="btn btn-warning btn-lg">Atualizar</button>
+                <a href="{{ route('clientes.index') }}" class="btn btn-danger btn-lg">Cancelar</a>
             </div>
     </div>
     </form>
